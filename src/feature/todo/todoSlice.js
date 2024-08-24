be80ba79bea600f9db525d3ea59c30649a61e19a
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-08-24 09:36:10 +0530

    content update

---
+++ src/feature/todo/todoSlice.js
@@ -0,0 +1,15 @@
+import {createSlice, nanoid} from "@reduxjs/toolkit";
+
+const initialState = {
+    todos: []
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state,action) => {
+
+        }
+    }
+});
