d533899e63655b7d42f221619e4fb03e4426ad65
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-08-24 09:45:40 +0530

    content update

--- src/feature/todo/todoSlice.js
+++ src/feature/todo/todoSlice.js
@@ -5,11 +5,19 @@ const initialState = {
 }
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: (state,action) => {
-
+    name: 'todo', initialState, reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(), title: action.payload,
+            }
+            state.todos.push(todo);
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
         }
     }
 });
+
+export const {addTodo, removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
